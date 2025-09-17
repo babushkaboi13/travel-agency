d9a873854106a37fb98f8de647d976fca656987a
Студент группы IS <is@sielom.local>
2025-09-17 16:36:37 +0500

    Revert "Added file readme"
    This reverts commit 428fc4d390d4fc71bf8c7e1ee95a5e9bd323fc91.

--- Jora/Jorik/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class JorikConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Jorik'
--- Jora/Jorik/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render, HttpResponse
-
-
-def print_hello(request):
-    return HttpResponse("Hello World")
